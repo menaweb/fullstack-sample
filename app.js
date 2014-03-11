f7ec80d36bbec339c32a79559bec0c233f8833f2
Rafael Mena Bravo <menaweb@gmail.com>
2014-03-11 11:49:48 +0100

    Pull data from Mongo and display it

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/helloworld', routes.helloworld);
+app.get('/userlist', routes.userlist(db));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
